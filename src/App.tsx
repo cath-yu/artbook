f5957a5c0444180507128285c6a4e410dd002591
catherineyu0611 <yu359@mcmaster.ca>
2026-01-30 01:11:17 -0500

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import './App.css';
 
+import Profile from "./assets/cacawtus.png"
+import Moon from "./assets/Moon.svg"
+
 function App() {
     return (
         <div className="main-page">
-            <p>artbook</p>
+            <div className="header">
+                <div className="profile">
+                    <div className="profile-picture">
+                        <img src={Profile}/>
+                    </div>
+                    <h2>cacaw</h2>
+                </div>
+
+                <div className="site-mode">
+                    <img src={Moon}/>
+                </div>
+            </div>
+
+            <h1>@cacawtus</h1>
         </div>
     )
 }
